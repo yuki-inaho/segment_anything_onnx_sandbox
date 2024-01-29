8bce6e2a1ac48015c040fe89a114a275faa54ada
yuki-inaho <yoshikawa@inaho.co>
2024-01-29 22:40:55 +0900

    Refactor

--- main.py
+++ main.py
@@ -77,104 +77,111 @@ def generate_coords_and_labels_from_point(
     return onnx_coord, onnx_label
 
 
-# コマンドライン引数の設定
-parser = argparse.ArgumentParser(description="Image Segmentation Script")
-parser.add_argument(
-    "-i",
-    "--image_path",
-    type=str,
-    default="data/apple_5x4.jpg",
-    help="Path to the image",
-)
-parser.add_argument(
-    "--enc_model_path",
-    type=str,
-    default="models/image_encoder_vit_b.onnx",
-    help="Path to the encoder model",
-)
-parser.add_argument(
-    "--sam_model_path",
-    type=str,
-    default="models/sam_vit_b_01ec64.onnx",
-    help="Path to the SAM model",
-)
-parser.add_argument(
-    "--input_point",
-    type=str,
-    default="321,230",
-    help="Input point for segmentation in format x,y (e.g., 321,230)",
-)
+def main(image_path, enc_model_path, sam_model_path, input_point):
+    # 画像の読み込みと前処理
+    image = cv2.imread(image_path)
+    orig_image_width, orig_image_height = image.shape[1], image.shape[0]
 
-# 引数を解析
-args = parser.parse_args()
+    # 画像のリサイズとパディング
+    image_processed, pad_h, resize_rate = resize_and_pad(image, SIZE_FOR_INFERENCE)
+    resized_image_width = int(orig_image_width * resize_rate)
 
-input_points = tuple(map(int, args.input_point.split(",")))
+    # エンコーダモデルのロード
+    encoder = ort.InferenceSession(
+        enc_model_path, providers=["CUDAExecutionProvider", "CPUExecutionProvider"]
+    )
+    image_tensor = as_normalized_float_tensor(
+        image_processed, pad_h, MEAN_PIX_VALUES, STD_PIX_VALUES
+    )
 
-# 画像の読み込みと前処理
-image = cv2.imread(args.image_path)
-orig_image_width, orig_image_height = image.shape[1], image.shape[0]
+    # エンコーディング
+    embeddings = encoder.run(None, {"images": image_tensor})[0]
+
+    onnx_coord, onnx_label = generate_coords_and_labels_from_point(
+        INPUT_POINT[0], INPUT_POINT[1], resize_rate
+    )
+
+    # デコーダモデルのロード
+    decoder = ort.InferenceSession(
+        sam_model_path, providers=["CUDAExecutionProvider", "CPUExecutionProvider"]
+    )
+
+    # セグメンテーションの実行
+    onnx_mask_input = np.zeros((1, 1, 256, 256), dtype=np.float32)
+    onnx_has_mask_input = np.zeros(1, dtype=np.float32)
+    outputs = decoder.run(
+        None,
+        {
+            "image_embeddings": embeddings,
+            "point_coords": onnx_coord,
+            "point_labels": onnx_label,
+            "mask_input": onnx_mask_input,
+            "has_mask_input": onnx_has_mask_input,
+            "orig_im_size": np.array(
+                [resized_image_width, resized_image_width], dtype=np.float32
+            ),
+        },
+    )
+    masks = outputs[0]
+
+    # マスクの処理
+    mask = masks[0][0]
+    mask = (mask > 0).astype("uint8") * 255
+    mask_cropped = mask[pad_h:-pad_h]
+    mask_rawsize = cv2.resize(
+        mask_cropped,
+        (orig_image_width, orig_image_height),
+        interpolation=cv2.INTER_NEAREST,
+    )
+
+    # オーバーレイの作成
+    mask_rgb = np.ones((image.shape[0], image.shape[1], 3), dtype=np.uint8) * 1
+    where_nonzero = np.where(mask_rawsize > 0)
+    mask_rgb[where_nonzero[0], where_nonzero[1], :] = np.array([0, 255, 0])
+
+    result = cv2.addWeighted(image, 0.6, mask_rgb, 0.4, 2.2)
+
+    # 結果の表示
+    cv2.imshow("Segmentation Result", result)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
-# 画像のリサイズとパディング
-image_processed, pad_h, resize_rate = resize_and_pad(image, SIZE_FOR_INFERENCE)
-resized_image_width, resized_image_height = int(orig_image_width * resize_rate), int(
-    orig_image_height * resize_rate
-)
+if __name__ == "__main__":
+    # コマンドライン引数の設定
+    parser = argparse.ArgumentParser(description="Image Segmentation Script")
+    parser.add_argument(
+        "-i",
+        "--image_path",
+        type=str,
+        default="data/apple_5x4.jpg",
+        help="Path to the image",
+    )
+    parser.add_argument(
+        "--enc_model_path",
+        type=str,
+        default="models/image_encoder_vit_b.onnx",
+        help="Path to the encoder model",
+    )
+    parser.add_argument(
+        "--sam_model_path",
+        type=str,
+        default="models/sam_vit_b_01ec64.onnx",
+        help="Path to the SAM model",
+    )
+    parser.add_argument(
+        "--input_point",
+        type=str,
+        default="321,230",
+        help="Input point for segmentation in format x,y (e.g., 321,230)",
+    )
 
-# エンコーダモデルのロード
-encoder = ort.InferenceSession(
-    args.enc_model_path, providers=["CUDAExecutionProvider", "CPUExecutionProvider"]
-)
-image_tensor = as_normalized_float_tensor(
-    image_processed, pad_h, MEAN_PIX_VALUES, STD_PIX_VALUES
-)
+    # 引数を解析
+    args = parser.parse_args()
 
-# エンコーディング
-embeddings = encoder.run(None, {"images": image_tensor})[0]
+    image_path = str(Path(args.image_path))
+    enc_model_path = str(Path(args.enc_model_path))
+    sam_model_path = str(Path(args.sam_model_path))
+    input_points = tuple(map(int, args.input_point.split(",")))
 
-onnx_coord, onnx_label = generate_coords_and_labels_from_point(
-    INPUT_POINT[0], INPUT_POINT[1], resize_rate
-)
-
-# デコーダモデルのロード
-decoder = ort.InferenceSession(
-    args.sam_model_path, providers=["CUDAExecutionProvider", "CPUExecutionProvider"]
-)
-
-# セグメンテーションの実行
-onnx_mask_input = np.zeros((1, 1, 256, 256), dtype=np.float32)
-onnx_has_mask_input = np.zeros(1, dtype=np.float32)
-outputs = decoder.run(
-    None,
-    {
-        "image_embeddings": embeddings,
-        "point_coords": onnx_coord,
-        "point_labels": onnx_label,
-        "mask_input": onnx_mask_input,
-        "has_mask_input": onnx_has_mask_input,
-        "orig_im_size": np.array(
-            [resized_image_width, resized_image_width], dtype=np.float32
-        ),
-    },
-)
-masks = outputs[0]
-
-# マスクの処理
-mask = masks[0][0]
-mask = (mask > 0).astype("uint8") * 255
-mask_cropped = mask[pad_h:-pad_h]
-mask_rawsize = cv2.resize(
-    mask_cropped, (orig_image_width, orig_image_height), interpolation=cv2.INTER_NEAREST
-)
-
-# オーバーレイの作成
-mask_rgb = np.ones((image.shape[0], image.shape[1], 3), dtype=np.uint8) * 1
-where_nonzero = np.where(mask_rawsize > 0)
-mask_rgb[where_nonzero[0], where_nonzero[1], :] = np.array([0, 255, 0])
-
-result = cv2.addWeighted(image, 0.6, mask_rgb, 0.4, 2.2)
-
-# 結果の表示
-cv2.imshow("Segmentation Result", result)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    main(image_path, enc_model_path, sam_model_path, input_points)
